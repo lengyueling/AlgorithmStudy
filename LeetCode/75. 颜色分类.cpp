72510871a19600fd7b6e169cca058b260fd20113
lengyueling <1030952490@qq.com>
2022-07-30 16:45:38 +0800

    add leetcode 167

--- LeetCode/75. 颜色分类.cpp
+++ LeetCode/75. 颜色分类.cpp
@@ -79,5 +79,36 @@ public:
 
 //版本3:三路快排
 //时间复杂度O(n) 空间复杂度O(1) 只遍历了一遍
+/*
+class Solution
+{
+public:
+	void sortColors(vector<int>& nums)
+	{
+		//num[i..two-1] == 1
+		int zero = -1;			//nums[0...zero] == 0
+		int two = nums.size();	//num[two...n-1] == 2
+		for(int i = 0; i < two;)
+		{
+			if(nums[i] == 1)
+			{
+				i++;
+			}
+			else if(nums[i] == 2)
+			{
+				two--;
+				swap(nums[i], nums[two]);
+			}
+			else if(nums[i] == 0)
+			{
+				zero++;
+				swap(nums[zero], nums[i]);
+				i++;
+			}
+		}
+	}
+};
+*/
+
 //拓展: 88 215题
 
