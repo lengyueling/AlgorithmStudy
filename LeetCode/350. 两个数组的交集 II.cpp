6a9d8e1593e4671ff074639554b4187a6c312c21
lengyueling <1030952490@qq.com>
2022-07-31 22:03:53 +0800

    fix leetcode 350

--- LeetCode/350. 两个数组的交集 II.cpp
+++ LeetCode/350. 两个数组的交集 II.cpp
@@ -14,17 +14,59 @@ class Solution
 			//填充record,每出现一次频次+1
 			for(int i = 0; i < nums1.size(); i++)
 			{
-				record[nums1[i]]++;
+				//元素不存在
+				if(record.find(nums1[i]) == record.end())
+				{
+					record.insert(make_pair(nums1[i], 1));
+				}
+				else
+				{
+					record[nums1[i]]++;
+				}
+				
 			}
 			vector<int> resultVec;
 			for(int i = 0; i < nums2.size(); i++)
 			{
-				if(record[nums2[i]] > 0)
+				//如果元素存在且频次>0
+				if(record.find(nums2[i]) != record.end() && record[nums2[i]] > 0)
 				{
 					resultVec.push_back(nums2[i]);
 					record[nums2[i]]--;
+					if(record[nums2[i]] == 0)
+					{
+						record.erase(nums2[i]);
+					}
 				}
 			}
 			return resultVec;
 		}
 };
+
+//简洁版本:
+/*
+class Solution
+{
+public:
+	vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
+	{
+		//元素 频次
+		map<int,int> record;
+		//填充record,每出现一次频次+1
+		for(int i = 0; i < nums1.size(); i++)
+		{
+			record[nums1[i]]++;
+		}
+		vector<int> resultVec;
+		for(int i = 0; i < nums2.size(); i++)
+		{
+			if(record[nums2[i]] > 0)
+			{
+				resultVec.push_back(nums2[i]);
+				record[nums2[i]]--;
+			}
+		}
+		return resultVec;
+	}
+};
+*/
