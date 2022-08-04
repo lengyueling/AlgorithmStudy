60be3088b3edccc5828aff76b1ef27ea18d9eb66
lengyueling <1030952490@qq.com>
2022-08-04 21:42:34 +0800

    fix leetcode 279

--- LeetCode/279. 完全平方数.cpp
+++ LeetCode/279. 完全平方数.cpp
@@ -5,7 +5,49 @@
 using namespace std;
 
 //这个问题无法使用贪心算法来解决
-//将这个问题转换为一个图论的问题,变成最短路径的问题
+//将这个问题转换为一个图论的问题,变成BFS最短路径的问题
+
+//优化:
+class Solution
+{
+public:
+	int numSquares(int n)
+	{
+		queue<pair<int, int>> q;
+		q.push(make_pair(n, 0));
+		vector<bool> visited(n+1, false);
+		visited[n] = false;
+		
+		while(!q.empty())
+		{
+			int num = q.front().first;
+			int step = q.front().second;
+			q.pop();
+			
+			for(int i = 1; ; i++)
+			{
+				int a = num - i * i;
+				if(a < 0)
+				{
+					break;
+				}
+				if(a == 0)
+				{
+					return step + 1;
+				}
+				if(!visited[a])
+				{
+					q.push(make_pair(a, step + 1));
+					visited[a] = true;
+				}
+			}
+		}
+		return -1;
+	}
+};
+
+//原版:
+/*
 class Solution
 {
 	public:
@@ -39,3 +81,6 @@ class Solution
 			return -1;
 		}
 };
+*/
+
+//拓展:127 126
