29ccdedf352bec8f4463a431dbda7a4ffc103755
lengyueling <1030952490@qq.com>
2022-08-07 01:06:55 +0800

    add LeetCode/509. 斐波那契数.cpp

---
+++ LeetCode/509. 斐波那契数.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+class Solution
+{
+	public:
+		int fib(int n)
+		{
+			if (n == 0)
+			{
+				return 0;
+			}
+			if (n == 1)
+			{
+				return 1;
+			}
+			return fib(n - 1) + fib(n - 2);
+		}
+};
